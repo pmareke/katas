025ae5fdfa3f821d688424f2aab5483dddce16ff
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2022-04-02 21:37:32 +0200

    feat: solved StringCalculator

--- string-calculator/string_calculator.py
+++ string-calculator/string_calculator.py
@@ -1,4 +1,20 @@
+import re
+
+
 class StringCalculator:
     @staticmethod
-    def add(x: int, y: int) -> int:
-        return 2
+    def add(input: str) -> int:
+        if input == "":
+            return 0
+
+        result = 0
+        pattern = re.compile("(-?[0-9]){1,}")
+        operators = re.finditer(pattern, input)
+        for op in operators:
+            num = int(op.group(0))
+            if num < 0:
+                raise Exception
+            if num <= 1000:
+                result += num
+
+        return result
--- string-calculator/string_calculator_test.py
+++ string-calculator/string_calculator_test.py
@@ -1,6 +1,32 @@
 from string_calculator import StringCalculator
+import pytest
 
 
 class TestStringCalculator:
-    def test_sum_two_numbers(self):
-        assert StringCalculator().add(1, 1) == 2
+    @pytest.mark.parametrize(
+        "input,result",
+        [
+            ("", 0),
+            ("4", 4),
+            ("1,2", 3),
+            ("1,2,3,4,5,6,7,8,9", 45),
+            ("1\n2,3", 6),
+            ("//;\n1;2", 3),
+            ("1001,2", 2),
+            ("//[***]\n1***2***3", 6),
+            ("//[*][%]\n1*2%3", 6),
+            ("//[foo][bar]\n1foo2bar3", 6),
+        ],
+    )
+    def test_sum_two_numbers(self, input, result):
+        assert StringCalculator().add(input) == result
+
+    @pytest.mark.parametrize(
+        "input",
+        [
+            "1,-2,-3",
+        ],
+    )
+    def test_sum_negative_numbers_raises_an_exception(self, input):
+        with pytest.raises(Exception):
+            StringCalculator().add(input)

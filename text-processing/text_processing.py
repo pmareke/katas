e9212e425cfae2e3c9dac6414c98f86d721322eb
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2022-04-03 11:54:20 +0200

    feat: solved TextProcessing

--- text-processing/text_processing.py
+++ text-processing/text_processing.py
@@ -4,22 +4,34 @@ import re
 
 class TextProcessing:
     @staticmethod
-    def process(input: str) -> str:
-        lines = ["Those are the top 10 words used:", ""]
+    def process(input: str, escape=None) -> str:
+        result = ["Those are the top 10 words used:", ""]
         seen: Dict = dict()
 
-        pattern = re.compile(r"\w+")
-        words = re.findall(pattern, input)
-        for word in words:
-            key = word.lower()
-            seen[key] = seen.get(key, 0) + 1
+        number_of_words = 0
+        lines = input.split("\n")
+        indices = []
+        for index, line in enumerate(lines):
+            if "```" in line:
+                indices.append(index)
+
+        if len(indices) > 0:
+            lines = lines[0 : indices[0]] + lines[indices[1] + 1 :]
+
+        for line in lines:
+            pattern = re.compile(r"\w+")
+            words = re.findall(pattern, line)
+            number_of_words += len(words)
+            for word in words:
+                key = word.lower()
+                seen[key] = seen.get(key, 0) + 1
 
         sort = dict(sorted(seen.items(), key=lambda item: item[1], reverse=True))
         for index, key in enumerate(sort.keys()):
             if index < 10:
-                lines.append(f"{index + 1}. {key}")
+                result.append(f"{index + 1}. {key}")
 
-        lines.append("")
-        lines.append(f"The text has in total {len(words)} words")
+        result.append("")
+        result.append(f"The text has in total {number_of_words} words")
 
-        return "\n".join(lines)
+        return "\n".join(result)
--- text-processing/text_processing_test.py
+++ text-processing/text_processing_test.py
@@ -1,4 +1,5 @@
 from text_processing import TextProcessing
+import pytest
 
 
 class TestTextProcessing:
@@ -21,3 +22,28 @@ class TestTextProcessing:
 
 The text has in total 21 words"""
         assert TextProcessing().process(input) == expected
+
+    def test_process_text_with_escapes(self):
+        input = """Hello, this is an example for you to practice. You should grab
+this text and make it as your test case:
+
+```javascript
+if (true) {
+  console.log('should should should')
+}
+```"""
+        expected = """Those are the top 10 words used:
+
+1. this
+2. you
+3. hello
+4. is
+5. an
+6. example
+7. for
+8. to
+9. practice
+10. should
+
+The text has in total 21 words"""
+        assert TextProcessing().process(input, "```") == expected

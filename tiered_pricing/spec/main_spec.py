9c1f3eaf9ce73532794f637822f564a9c66272f0
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2022-05-27 19:59:37 +0200

    feat: solved tiered price

--- tiered_pricing/spec/main_spec.py
+++ tiered_pricing/spec/main_spec.py
@@ -1,6 +1,31 @@
 from mamba import description, it
-from expects import expect, be_true
+from expects import expect, equal, raise_error
+from src.main import Subscription, InvalidSubscriptionNumber
 
-with description('DummyClass') as self:
-    with it('#dummy'):
-        expect(True).to(be_true)
+with description("Tiered Pricing") as self:
+    with it("#total_price"):
+
+        tt = [
+            {"subscriptions": 1, "total": 299},
+            {"subscriptions": 2, "total": 598},
+            {"subscriptions": 3, "total": 717},
+            {"subscriptions": 4, "total": 956},
+            {"subscriptions": 5, "total": 1195},
+            {"subscriptions": 11, "total": 2409},
+            {"subscriptions": 12, "total": 2628},
+            {"subscriptions": 13, "total": 2847},
+            {"subscriptions": 26, "total": 5174},
+            {"subscriptions": 27, "total": 5373},
+            {"subscriptions": 28, "total": 5572},
+            {"subscriptions": 50, "total": 9950},
+            {"subscriptions": 51, "total": 7599},
+            {"subscriptions": 52, "total": 7748},
+        ]
+
+        for tc in tt:
+            subscription = Subscription(tc["subscriptions"])
+
+            expect(subscription.calculate_total_price()).to(equal(tc["total"]))
+
+    with it("an invalid number of subscriptions"):
+        expect(lambda: Subscription(-1)).to(raise_error(InvalidSubscriptionNumber))
---
+++ tiered_pricing/src/main.py
@@ -0,0 +1,30 @@
+import sys
+
+
+class InvalidSubscriptionNumber(Exception):
+    pass
+
+
+class Subscription:
+    def __init__(self, subscriptions: int) -> None:
+        if subscriptions <= 0:
+            raise InvalidSubscriptionNumber()
+        self.subscriptions = subscriptions
+
+    def calculate_total_price(self) -> int:
+        return self.subscriptions * self.unit_price()
+
+    # PRIVATE
+
+    def unit_price(self) -> int:
+        ranges = [
+            {"min": 1, "max": 2, "unit_price": 299},
+            {"min": 3, "max": 10, "unit_price": 239},
+            {"min": 11, "max": 25, "unit_price": 219},
+            {"min": 26, "max": 50, "unit_price": 199},
+        ]
+
+        for r in ranges:
+            if self.subscriptions in range(r["min"], r["max"] + 1):
+                return r["unit_price"]
+        return 149

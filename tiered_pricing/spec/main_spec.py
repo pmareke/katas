381196d11d2c90e637966880ff52657e95fc1a0c
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2022-05-31 19:07:27 +0200

    solved graduated version

--- tiered_pricing/spec/main_spec.py
+++ tiered_pricing/spec/main_spec.py
@@ -1,9 +1,9 @@
 from mamba import description, it
 from expects import expect, equal, raise_error
-from src.main import Subscription, InvalidSubscriptionNumber
+from src.main import Subscription, GraduatedSubscription, InvalidSubscriptionNumber
 
 with description("Tiered Pricing") as self:
-    with it("a valid numnber of subscriptions"):
+    with it("a valid number of subscriptions"):
         tt = [
             {"subscriptions": 1, "total": 299},
             {"subscriptions": 2, "total": 598},
@@ -23,6 +23,33 @@ with description("Tiered Pricing") as self:
 
         for tc in tt:
             subscription = Subscription(tc["subscriptions"])
+
+            expect(subscription.calculate_total_price()).to(equal(tc["total"]))
+
+    with it("an invalid number of subscriptions"):
+        expect(lambda: Subscription(-1)).to(raise_error(InvalidSubscriptionNumber))
+
+with description("Graduated Tiered Pricing") as self:
+    with it("a valid number of subscriptions"):
+        tt = [
+            {"subscriptions": 1, "total": 299},
+            {"subscriptions": 2, "total": 598},
+            {"subscriptions": 3, "total": 837},
+            {"subscriptions": 4, "total": 1076},
+            {"subscriptions": 5, "total": 1315},
+            {"subscriptions": 11, "total": 2729},
+            {"subscriptions": 12, "total": 2948},
+            {"subscriptions": 13, "total": 3167},
+            {"subscriptions": 26, "total": 5994},
+            {"subscriptions": 27, "total": 6193},
+            {"subscriptions": 28, "total": 6392},
+            {"subscriptions": 50, "total": 10770},
+            {"subscriptions": 51, "total": 10919},
+            {"subscriptions": 52, "total": 11068},
+        ]
+
+        for tc in tt:
+            subscription = GraduatedSubscription(tc["subscriptions"])
 
             expect(subscription.calculate_total_price()).to(equal(tc["total"]))
 
--- tiered_pricing/src/main.py
+++ tiered_pricing/src/main.py
@@ -5,6 +5,9 @@ class InvalidSubscriptionNumber(Exception):
     pass
 
 
+MINIMUM_UNIT_PRICE = 149
+
+
 class Subscription:
     def __init__(self, subscriptions: int) -> None:
         if subscriptions <= 0:
@@ -23,9 +26,36 @@ class Subscription:
             {"min": 11, "max": 25, "unit_price": 219},
             {"min": 26, "max": 50, "unit_price": 199},
         ]
-        MINIMUM_UNIT_PRICE = 149
 
         for entry in unit_price_by_number_of_subscriptions:
             if self.subscriptions in range(entry["min"], entry["max"] + 1):
+                return entry["unit_price"]
+        return MINIMUM_UNIT_PRICE
+
+
+class GraduatedSubscription:
+    def __init__(self, subscriptions: int) -> None:
+        if subscriptions <= 0:
+            raise InvalidSubscriptionNumber()
+        self.subscriptions = subscriptions
+
+    def calculate_total_price(self) -> int:
+        return sum(
+            self.unit_price(subscription_index)
+            for subscription_index in range(1, self.subscriptions + 1)
+        )
+
+    # PRIVATE
+
+    def unit_price(self, subscription_index: int) -> int:
+        unit_price_by_number_of_subscriptions = [
+            {"min": 1, "max": 2, "unit_price": 299},
+            {"min": 3, "max": 10, "unit_price": 239},
+            {"min": 11, "max": 25, "unit_price": 219},
+            {"min": 26, "max": 50, "unit_price": 199},
+        ]
+
+        for entry in unit_price_by_number_of_subscriptions:
+            if subscription_index in range(entry["min"], entry["max"] + 1):
                 return entry["unit_price"]
         return MINIMUM_UNIT_PRICE

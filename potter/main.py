97c24d71cebda9b2abcd0fdcf1f3a076c1a11a73
Pedro Lopez Mareque <pedro.lopez@clarity.ai>
2022-06-06 11:41:04 +0200

    feat: create private method and update tests

--- potter/main.py
+++ potter/main.py
@@ -2,11 +2,13 @@ from typing import List
 
 
 class Potter:
+    def __init__(self, items: List[int]) -> None:
+        self.items = items
+
+    def price(self) -> int:
+        return self._calculate_total_price(self.items)
+
     @staticmethod
-    def price(items: List[int]) -> int:
+    def _calculate_total_price(items: List[int]) -> int:
         UNIT_PRICE = 8
-
-        if len(items) == 0:
-            return 0
-
         return sum([UNIT_PRICE * qty for qty in items])
--- potter/main_test.py
+++ potter/main_test.py
@@ -12,8 +12,13 @@ from main import Potter
         (8, [1]),
         (8, [1]),
         (8 * 3, [1, 1, 1]),
+        (8 * 2 * 0.95, [0, 1]),
+        (8 * 3 * 0.9, [0, 2, 4]),
+        (8 * 4 * 0.8, [0, 1, 2, 4]),
+        (8 * 5 * 0.75, [0, 1, 2, 3, 4]),
     ],
 )
 class TestPotter:
     def test_basics(self, price: int, items: List[int]) -> None:
-        assert price == Potter.price(items)
+        potter = Potter(items)
+        assert price == potter.price()

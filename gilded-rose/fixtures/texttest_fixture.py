36187eea37679e8ba059049be19f714d5c8ee22b
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2022-08-03 22:48:09 +0200

    feat: add Makefile (WIP)

--- gilded-rose/fixtures/texttest_fixture.py
+++ gilded-rose/fixtures/texttest_fixture.py
@@ -1,7 +1,7 @@
-# -*- coding: utf-8 -*-
 from __future__ import print_function
 
-from gilded_rose import *
+from app.gilded_rose import GildedRose
+from app.domain.item import Item
 
 if __name__ == "__main__":
     print("OMGHAI!")

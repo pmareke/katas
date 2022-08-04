f270beccbce6d9667e4b2ead602528db3719fdd9
Pedro Lopez Mareque <pedro.lopez@clarity.ai>
2022-08-04 08:50:18 +0200

    feat: remove pytest cache and refactor code

--- gilded-rose/app/domain/item.py
+++ gilded-rose/app/domain/item.py
@@ -1,11 +1,11 @@
 from dataclasses import dataclass
 
 
-@dataclass(repr=False)
+@dataclass(repr=False, eq=False)
 class Item:
     name: str
     sell_in: int
     quality: int
 
-    def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+    def __repr__(self) -> str:
+        return f"{self.name}, {self.sell_in}, {self.quality}"
--- gilded-rose/tests/test_gilded_rose.py
+++ gilded-rose/tests/test_gilded_rose.py
@@ -1,3 +1,4 @@
+import pytest
 from expects import expect, equal
 
 from app.domain.item import Item
@@ -5,10 +6,24 @@ from app.gilded_rose import GildedRose
 
 
 class TestGildedRose:
-    def test_updates_items(self) -> None:
-        items = [Item("foo", 0, 0)]
+    @pytest.mark.parametrize(
+        "item",
+        [
+            Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
+            Item(name="Aged Brie", sell_in=2, quality=0),
+            Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
+            Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+            Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
+            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
+            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+            Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
+        ],
+    )
+    def test_updates_item_quality(self, item: Item) -> None:
+        items = [item]
         gilded_rose = GildedRose(items)
 
         gilded_rose.update_quality()
 
-        expect("foo").to(equal(items[0].name))
+        expect(0).to(equal(items[0].quality))

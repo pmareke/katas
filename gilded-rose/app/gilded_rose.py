f1b6a439767aa5ae42a5ceb5305b79f9b38c4fe0
Pedro Lopez Mareque <pedro.lopez@clarity.ai>
2022-08-05 09:26:35 +0200

    refactor: add approval tests

--- gilded-rose/app/gilded_rose.py
+++ gilded-rose/app/gilded_rose.py
@@ -1,5 +1,6 @@
-from typing import List
 from dataclasses import dataclass
+from typing import List
+
 from app.domain.item import Item
 
 
@@ -9,30 +10,33 @@ class GildedRose:
 
     def update_quality(self) -> None:
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
+            if item.name == "Aged Brie" or item.name == "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+                    self._item_quality_under_50(item)
+            else:
+                self._item_name_not_aged_brie_or_backstage(item)
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                self._sell_in_under_0(item)
+
+    @staticmethod
+    def _item_quality_under_50(item: Item) -> None:
+        item.quality = item.quality + 1
+        if item.name == "Backstage passes to a TAFKAL80ETC concert" and item.sell_in < 11 and item.quality < 50:
+            item.quality = item.quality + 1
+
+    @staticmethod
+    def _item_name_not_aged_brie_or_backstage(item: Item) -> None:
+        if item.quality > 0 and item.name != "Sulfuras, Hand of Ragnaros":
+            item.quality = item.quality - 1
+
+    def _sell_in_under_0(self, item: Item) -> None:
+        if item.name != "Aged Brie":
+            if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                item.quality = item.quality - item.quality
+            else:
+                self._item_name_not_aged_brie_or_backstage(item)
+        else:
+            if item.quality < 50:
+                item.quality = item.quality + 1
--- gilded-rose/tests/test_gilded_rose.py
+++ gilded-rose/tests/test_gilded_rose.py
@@ -1,128 +1,22 @@
-import pytest
-from expects import expect, equal
-
+from approvaltests.combination_approvals import verify_all_combinations
 from app.domain.item import Item
 from app.gilded_rose import GildedRose
 
 
 class TestGildedRose:
-    @pytest.mark.parametrize(
-        "item,quantity",
-        [
-            (Item(name="+5 Dexterity Vest", sell_in=10, quality=20), 19),
-            (Item(name="Aged Brie", sell_in=2, quality=0), 1),
-            (Item(name="Elixir of the Mongoose", sell_in=5, quality=7), 6),
-            (Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80), 80),
-            (Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80), 80),
-            (Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20), 21),
-            (Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49), 50),
-            (Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49), 50),
-            (Item(name="Conjured Mana Cake", sell_in=3, quality=6), 5),  # <-- :O
-        ],
-    )
-    def test_updates_item_quality_one_time(self, item: Item, quantity: int) -> None:
-        items = [item]
+    def test_update_quality(self) -> None:
+        verify_all_combinations(
+            self._update_quality,
+            [
+                ["foo"],
+                [0],
+                [0],
+            ],
+        )
+
+    @staticmethod
+    def _update_quality(name: str, quality: int, sell_in: int) -> str:
+        items = [Item(name, sell_in, quality)]
         gilded_rose = GildedRose(items)
-
         gilded_rose.update_quality()
-
-        expect(quantity).to(equal(items[0].quality))
-
-    @pytest.mark.parametrize(
-        "item,quantity",
-        [
-            (Item(name="+5 Dexterity Vest", sell_in=10, quality=20), 18),
-            (Item(name="Aged Brie", sell_in=2, quality=0), 2),
-            (Item(name="Elixir of the Mongoose", sell_in=5, quality=7), 5),
-            (Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80), 80),
-            (Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80), 80),
-            (Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20), 22),
-            (Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49), 50),
-            (Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49), 50),
-            (Item(name="Conjured Mana Cake", sell_in=3, quality=6), 4),  # <-- :O
-        ],
-    )
-    def test_updates_item_quality_two_times(self, item: Item, quantity: int) -> None:
-        items = [item]
-        gilded_rose = GildedRose(items)
-
-        gilded_rose.update_quality()
-        gilded_rose.update_quality()
-
-        expect(quantity).to(equal(items[0].quality))
-
-    @pytest.mark.parametrize(
-        "item,quantity",
-        [
-            (Item(name="+5 Dexterity Vest", sell_in=10, quality=20), 17),
-            (Item(name="Aged Brie", sell_in=2, quality=0), 4),
-            (Item(name="Elixir of the Mongoose", sell_in=5, quality=7), 4),
-            (Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80), 80),
-            (Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80), 80),
-            (Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20), 23),
-            (Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49), 50),
-            (Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49), 50),
-            (Item(name="Conjured Mana Cake", sell_in=3, quality=6), 3),  # <-- :O
-        ],
-    )
-    def test_updates_item_quality_three_times(self, item: Item, quantity: int) -> None:
-        items = [item]
-        gilded_rose = GildedRose(items)
-
-        gilded_rose.update_quality()
-        gilded_rose.update_quality()
-        gilded_rose.update_quality()
-
-        expect(quantity).to(equal(items[0].quality))
-
-    @pytest.mark.parametrize(
-        "item,quantity",
-        [
-            (Item(name="+5 Dexterity Vest", sell_in=10, quality=20), 16),
-            (Item(name="Aged Brie", sell_in=2, quality=0), 6),
-            (Item(name="Elixir of the Mongoose", sell_in=5, quality=7), 3),
-            (Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80), 80),
-            (Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80), 80),
-            (Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20), 24),
-            (Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49), 50),
-            (Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49), 50),
-            (Item(name="Conjured Mana Cake", sell_in=3, quality=6), 1),  # <-- :O
-        ],
-    )
-    def test_updates_item_quality_four_times(self, item: Item, quantity: int) -> None:
-        items = [item]
-        gilded_rose = GildedRose(items)
-
-        gilded_rose.update_quality()
-        gilded_rose.update_quality()
-        gilded_rose.update_quality()
-        gilded_rose.update_quality()
-
-        expect(quantity).to(equal(items[0].quality))
-
-    @pytest.mark.parametrize(
-        "item,quantity",
-        [
-            (Item(name="+5 Dexterity Vest", sell_in=10, quality=20), 14),
-            (Item(name="Aged Brie", sell_in=2, quality=0), 10),
-            (Item(name="Elixir of the Mongoose", sell_in=5, quality=7), 0),
-            (Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80), 80),
-            (Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80), 80),
-            (Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20), 27),
-            (Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49), 50),
-            (Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49), 0),
-            (Item(name="Conjured Mana Cake", sell_in=3, quality=6), 0),  # <-- :O
-        ],
-    )
-    def test_updates_item_quality_six_times(self, item: Item, quantity: int) -> None:
-        items = [item]
-        gilded_rose = GildedRose(items)
-
-        gilded_rose.update_quality()
-        gilded_rose.update_quality()
-        gilded_rose.update_quality()
-        gilded_rose.update_quality()
-        gilded_rose.update_quality()
-        gilded_rose.update_quality()
-
-        expect(quantity).to(equal(items[0].quality))
+        return f"{gilded_rose.items[0]}"

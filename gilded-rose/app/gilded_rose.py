944d4b3d913b15326efbb687c01ea5b59804bb88
Pedro Lopez Mareque <pedro.lopez@clarity.ai>
2022-08-08 08:25:03 +0200

    refactor to static methods

--- gilded-rose/app/gilded_rose.py
+++ gilded-rose/app/gilded_rose.py
@@ -16,7 +16,8 @@ class GildedRose:
             elif item.name != "Sulfuras, Hand of Ragnaros":
                 self.normal_item(item)
 
-    def normal_item(self, item):
+    @staticmethod
+    def normal_item(item):
         if item.quality > 0:
             item.quality = item.quality - 1
         item.sell_in = item.sell_in - 1
@@ -24,7 +25,8 @@ class GildedRose:
             if item.quality > 0:
                 item.quality = item.quality - 1
 
-    def backstage(self, item):
+    @staticmethod
+    def backstage(item):
         if item.quality < 50:
             item.quality = item.quality + 1
             if item.sell_in < 11:

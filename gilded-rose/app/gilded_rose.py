601b08306e9c2c7e29d82a0020307fb3ecb3c117
Pedro Lopez Mareque <pedro.lopez@clarity.ai>
2022-08-08 08:22:35 +0200

    extract backstage logic

--- gilded-rose/app/gilded_rose.py
+++ gilded-rose/app/gilded_rose.py
@@ -11,34 +11,31 @@ class GildedRose:
         for item in self.items:
             if item.name == "Aged Brie":
                 self.aged_brie(item)
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                self.backstage(item)
             else:
-                if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                    if item.quality > 0:
-                        if item.name != "Sulfuras, Hand of Ragnaros":
-                            item.quality = item.quality - 1
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-                        if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                            if item.sell_in < 11:
-                                if item.quality < 50:
-                                    item.quality = item.quality + 1
-                            if item.sell_in < 6:
-                                if item.quality < 50:
-                                    item.quality = item.quality + 1
+                if item.quality > 0:
+                    if item.name != "Sulfuras, Hand of Ragnaros":
+                        item.quality = item.quality - 1
                 if item.name != "Sulfuras, Hand of Ragnaros":
                     item.sell_in = item.sell_in - 1
                 if item.sell_in < 0:
-                    if item.name != "Aged Brie":
-                        if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                            if item.quality > 0:
-                                if item.name != "Sulfuras, Hand of Ragnaros":
-                                    item.quality = item.quality - 1
-                        else:
-                            item.quality = item.quality - item.quality
-                    else:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
+                    if item.quality > 0:
+                        if item.name != "Sulfuras, Hand of Ragnaros":
+                            item.quality = item.quality - 1
+
+    def backstage(self, item):
+        if item.quality < 50:
+            item.quality = item.quality + 1
+            if item.sell_in < 11:
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+            if item.sell_in < 6:
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+        item.sell_in = item.sell_in - 1
+        if item.sell_in < 0:
+            item.quality = item.quality - item.quality
 
     @staticmethod
     def aged_brie(item):

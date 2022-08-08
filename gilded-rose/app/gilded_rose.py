67dc739e1810046405d6e228377c3a4f7aa1959f
Pedro Lopez Mareque <pedro.lopez@clarity.ai>
2022-08-08 08:25:47 +0200

    refactor: remove unnecessary if in the aged brie method

--- gilded-rose/app/gilded_rose.py
+++ gilded-rose/app/gilded_rose.py
@@ -43,8 +43,7 @@ class GildedRose:
     def aged_brie(item):
         if item.quality < 50:
             item.quality = item.quality + 1
-        if item.name != "Sulfuras, Hand of Ragnaros":
-            item.sell_in = item.sell_in - 1
+        item.sell_in = item.sell_in - 1
         if item.sell_in < 0:
             if item.quality < 50:
                 item.quality = item.quality + 1

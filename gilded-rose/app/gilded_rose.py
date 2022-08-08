2d695fe2d8ad90f1c94647738f35bd3d56874ac2
Pedro Lopez Mareque <pedro.lopez@clarity.ai>
2022-08-08 08:24:31 +0200

    extract normal logic

--- gilded-rose/app/gilded_rose.py
+++ gilded-rose/app/gilded_rose.py
@@ -13,16 +13,16 @@ class GildedRose:
                 self.aged_brie(item)
             elif item.name == "Backstage passes to a TAFKAL80ETC concert":
                 self.backstage(item)
-            else:
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-                if item.name != "Sulfuras, Hand of Ragnaros":
-                    item.sell_in = item.sell_in - 1
-                if item.sell_in < 0:
-                    if item.quality > 0:
-                        if item.name != "Sulfuras, Hand of Ragnaros":
-                            item.quality = item.quality - 1
+            elif item.name != "Sulfuras, Hand of Ragnaros":
+                self.normal_item(item)
+
+    def normal_item(self, item):
+        if item.quality > 0:
+            item.quality = item.quality - 1
+        item.sell_in = item.sell_in - 1
+        if item.sell_in < 0:
+            if item.quality > 0:
+                item.quality = item.quality - 1
 
     def backstage(self, item):
         if item.quality < 50:

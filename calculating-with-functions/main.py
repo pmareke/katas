81389253a16bfc2f8f09efe3ff9c94b6afbaf791
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2022-08-22 21:38:37 +0200

    feat: add calculating with functions

--- calculating-with-functions/main.py
+++ calculating-with-functions/main.py
@@ -1,2 +1,36 @@
-class CalculatingWithFunctions:
-    pass
+from functools import partial
+from typing import Callable
+
+
+numbers = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
+
+for number, value in numbers.items():
+    exec(
+        f"def {number}(fn: Callable = lambda x: x) -> Callable: return partial(fn, {value})"
+    )
+
+
+def plus(fn: Callable) -> Callable:
+    return partial(lambda x, y: x + y, fn())  # type: ignore
+
+
+def minus(fn: Callable) -> Callable:
+    return partial(lambda x, y: y - x, fn())  # type: ignore
+
+
+def times(fn: Callable) -> Callable:
+    return partial(lambda x, y: x * y, fn())  # type: ignore
+
+
+def divided_by(fn: Callable) -> Callable:
+    return partial(lambda x, y: int(y / x), fn())  # type: ignore
--- calculating-with-functions/main_test.py
+++ calculating-with-functions/main_test.py
@@ -1,16 +1,32 @@
-from expects import be_true, expect
+from expects import be_true, expect, equal
 from typing import Callable
 import pytest
 
-from main import CalculatingWithFunctions
+from main import (
+    seven,
+    times,
+    five,
+    four,
+    plus,
+    nine,
+    eight,
+    minus,
+    three,
+    six,
+    divided_by,
+    two,
+)
 
 
 @pytest.mark.parametrize(
     "input,result",
     [
-        (),
+        (seven(times(five())), 35),
+        (four(plus(nine())), 13),
+        (eight(minus(three())), 5),
+        (six(divided_by(two())), 3),
     ],
 )
 class TestCalculatingWithFunctions:
-    def test_calculates_sum_of_pairs(self, input: Callable, result: int) -> None:
-        expect(True).to(be_true)
+    def test_calculates_functions(self, input: Callable, result: int) -> None:
+        expect(input()).to(equal(result))

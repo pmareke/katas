0d5c405b7e210cb75f3d6e701d840e64464401db
Pedro Lopez Mareque <pedro.lopez@clarity.ai>
2022-08-22 11:20:23 +0200

    refactor: simplify code

--- range-extraction/main.py
+++ range-extraction/main.py
@@ -6,22 +6,22 @@ class RangeExtraction:
         self.input = input
 
     def extract(self) -> str:
-        ranges: List[str] = []
+        string_ranges: List[str] = []
         current_range: List[int] = []
         for index, number in enumerate(self.input):
             if len(current_range) == 0:  # First element or after starting a new range
                 current_range = [number]
             if index == len(self.input) - 1:  # Last element
                 string_range = self._calculate_string_range(current_range)
-                ranges.append(string_range)
+                string_ranges.append(string_range)
                 break
             if number + 1 != self.input[index + 1]:  # Next element is not consecutive
                 string_range = self._calculate_string_range(current_range)
-                ranges.append(string_range)
+                string_ranges.append(string_range)
                 current_range = [self.input[index + 1]]
             else:  # Next number is consecutive
                 current_range.append(number + 1)
-        return ",".join(ranges)
+        return ",".join(string_ranges)
 
     def _calculate_string_range(self, range_list: List[int]) -> str:
         first = range_list[0]

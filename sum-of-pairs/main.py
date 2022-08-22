e4179bf5691351969914aa92916c03e262763f98
Pedro Lopez Mareque <pedro.lopez@clarity.ai>
2022-08-22 15:34:33 +0200

    refactor: add sum of pairs

--- sum-of-pairs/main.py
+++ sum-of-pairs/main.py
@@ -1,7 +1,19 @@
-from typing import List, Optional
+from typing import Dict, List, Optional
 
 
 class SumPairs:
     @staticmethod
-    def process(input: List[int], sum: int) -> Optional[List[int]]:
-        return input
+    def process(input: List[int], target: int) -> Optional[List[int]]:
+        pairs: Dict[int, List[int]] = {}
+        for x in range(0, len(input)):
+            for y in range(x + 1, len(input)):
+                x1 = input[x]
+                y1 = input[y]
+                if x1 + y1 == target:
+                    pairs[y] = [x1, y1]
+
+        if not pairs:
+            return None
+
+        smallest_index = sorted(pairs.keys())[0]
+        return pairs[smallest_index]
--- sum-of-pairs/main_test.py
+++ sum-of-pairs/main_test.py
@@ -7,9 +7,16 @@ from main import SumPairs
 
 
 @pytest.mark.parametrize(
-    "input,sum",
-    [([11, 3, 7, 5], 10)],
+    "input,target,result",
+    [
+        ([11, 3, 7, 5], 10, [3, 7]),
+        ([4, 3, 2, 3, 4], 6, [4, 2]),
+        ([0, 0, -2, 3], 2, None),
+        ([10, 5, 2, 3, 7, 5], 10, [3, 7]),
+    ],
 )
 class TestSumPairs:
-    def test_calculates_sum_of_pairs(self, input: List[int], sum: int) -> None:
-        expect(SumPairs.process(input, sum)).to(equal(sum))
+    def test_calculates_sum_of_pairs(
+        self, input: List[int], target: int, result: List[int]
+    ) -> None:
+        expect(SumPairs.process(input, target)).to(equal(result))

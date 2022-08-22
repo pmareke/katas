f23d545ecd466acbc4c04bf6e83e6c229cc4edcf
Pedro Lopez Mareque <pedro.lopez@clarity.ai>
2022-08-22 11:18:56 +0200

    refactor: remove ranges class property

--- range-extraction/main.py
+++ range-extraction/main.py
@@ -4,26 +4,28 @@ from typing import List
 class RangeExtraction:
     def __init__(self, input: List[int]):
         self.input = input
-        self._ranges: List[str] = []
 
     def extract(self) -> str:
+        ranges: List[str] = []
         current_range: List[int] = []
         for index, number in enumerate(self.input):
             if len(current_range) == 0:  # First element or after starting a new range
                 current_range = [number]
             if index == len(self.input) - 1:  # Last element
-                self._add_range(current_range)
+                string_range = self._calculate_string_range(current_range)
+                ranges.append(string_range)
                 break
             if number + 1 != self.input[index + 1]:  # Next element is not consecutive
-                self._add_range(current_range)
+                string_range = self._calculate_string_range(current_range)
+                ranges.append(string_range)
                 current_range = [self.input[index + 1]]
             else:  # Next number is consecutive
                 current_range.append(number + 1)
-        return ",".join(self._ranges)
+        return ",".join(ranges)
 
-    def _add_range(self, range_list: List[int]) -> None:
+    def _calculate_string_range(self, range_list: List[int]) -> str:
         first = range_list[0]
         last = range_list[-1]
         separator = "-" if len(range_list) > 2 else ","
-        out = f"{first}" if len(range_list) == 1 else f"{first}{separator}{last}"
-        self._ranges.append(out)
+        r = f"{first}" if len(range_list) == 1 else f"{first}{separator}{last}"
+        return r

bf7ceeb568c377541fc58366d85f037d410a85ac
Pedro Lopez Mareque <pedro.lopez@clarity.ai>
2022-09-07 16:05:01 +0200

    refactor: add missing decorator

--- bank/src/domain/printer.py
+++ bank/src/domain/printer.py
@@ -1,9 +1,10 @@
-from abc import ABC
+from abc import abstractmethod, ABC
 from typing import List
 
 from bank.src.domain.transaction import Transaction
 
 
 class Printer(ABC):
+    @abstractmethod
     def print(self, transactions: List[Transaction]) -> None:
         raise NotImplementedError

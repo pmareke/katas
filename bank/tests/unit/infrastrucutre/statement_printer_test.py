3e63f5f3feeaec6c4abb58f52948fc8c97879837
Pedro Lopez Mareque <pedro.lopez@clarity.ai>
2022-09-08 08:32:36 +0200

    refactor: improve statement printer tests

--- bank/tests/unit/infrastrucutre/statement_printer_test.py
+++ bank/tests/unit/infrastrucutre/statement_printer_test.py
@@ -1,5 +1,5 @@
-from doublex import Mimic, Spy
-from doublex_expects import have_been_called_with
+from doublex import Mimic, Mock
+from doublex_expects import have_been_satisfied
 from expects import expect
 
 from bank.src.domain.transaction import Transaction, TransactionType
@@ -7,68 +7,61 @@ from bank.src.infrastructure.console_output import Console
 from bank.src.infrastructure.statement_printer import StatementPrinter
 
 
+def deposit(day: str, amount: int) -> Transaction:
+    return Transaction(TransactionType.DEPOSIT, day, amount)
+
+
+def withdraw(day: str, amount: int) -> Transaction:
+    return Transaction(TransactionType.WITHDRAW, day, amount)
+
+
 class TestStatementPrinter:
     def test_prints_the_header(self) -> None:
-        console = Mimic(Spy, Console)
+        with Mimic(Mock, Console) as console:
+            console.print_line("date || credit || debit || balance")
+
         statement_printer = StatementPrinter(console)
 
         statement_printer.print([])
 
-        expect(console.print_line).to(
-            have_been_called_with("date || credit || debit || balance")
-        )
+        expect(console).to(have_been_satisfied)
 
     def test_prints_one_deposit_transaction(self) -> None:
-        console = Mimic(Spy, Console)
+        with Mimic(Mock, Console) as console:
+            console.print_line("date || credit || debit || balance")
+            console.print_line("12/01/2012 || 1000.00 || || 1000.00")
+
         statement_printer = StatementPrinter(console)
 
-        statement_printer.print(
-            [Transaction(TransactionType.DEPOSIT, "12/01/2012", 1000)]
-        )
+        statement_printer.print([deposit("12/01/2012", 1000)])
 
-        expect(console.print_line).to(
-            have_been_called_with("date || credit || debit || balance")
-        )
-        expect(console.print_line).to(
-            have_been_called_with("12/01/2012 || 1000.00 || || 1000.00")
-        )
+        expect(console).to(have_been_satisfied)
 
     def test_prints_one_withdraw_transaction(self) -> None:
-        console = Mimic(Spy, Console)
+        with Mimic(Mock, Console) as console:
+            console.print_line("date || credit || debit || balance")
+            console.print_line("12/01/2012 || || 1000.00 || -1000.00")
+
         statement_printer = StatementPrinter(console)
 
-        statement_printer.print(
-            [Transaction(TransactionType.WITHDRAW, "12/01/2012", 1000)]
-        )
+        statement_printer.print([withdraw("12/01/2012", 1000)])
 
-        expect(console.print_line).to(
-            have_been_called_with("date || credit || debit || balance")
-        )
-        expect(console.print_line).to(
-            have_been_called_with("12/01/2012 || || 1000.00 || -1000.00")
-        )
+        expect(console).to(have_been_satisfied)
 
     def test_prints_transactions_in_chronological_order(self) -> None:
-        console = Mimic(Spy, Console)
+        with Mimic(Mock, Console) as console:
+            console.print_line("date || credit || debit || balance")
+            console.print_line("14/01/2012 || 500.00 || || 1400.00")
+            console.print_line("13/01/2012 || || 100.00 || 900.00")
+            console.print_line("12/01/2012 || 1000.00 || || 1000.00")
         statement_printer = StatementPrinter(console)
 
         statement_printer.print(
             [
-                Transaction(TransactionType.DEPOSIT, "12/01/2012", 1000),
-                Transaction(TransactionType.WITHDRAW, "13/01/2012", 100),
-                Transaction(TransactionType.DEPOSIT, "14/01/2012", 500),
+                deposit("12/01/2012", 1000),
+                withdraw("13/01/2012", 100),
+                deposit("14/01/2012", 500),
             ]
         )
 
-        expect(console.print_line).to(
-            have_been_called_with("date || credit || debit || balance")
-        )
-        expect(console.print_line).to(
-            have_been_called_with("14/01/2012 || 500.00 || || 1400.00")
-        )
-        expect(console.print_line).to(
-            have_been_called_with("13/01/2012 || || 100.00 || 900.00")
-        )
-        expect(console.print_line).to(
-            have_been_called_with("12/01/2012 || 1000.00 || || 1000.00")
-        )
+        expect(console).to(have_been_satisfied)

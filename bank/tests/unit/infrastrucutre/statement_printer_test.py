c36805b358f69d2640345ab1652d3db55eb9256b
Pedro Lopez Mareque <pedro.lopez@clarity.ai>
2022-09-08 08:37:28 +0200

    refactor: improve statement printer tests

--- bank/tests/unit/infrastrucutre/statement_printer_test.py
+++ bank/tests/unit/infrastrucutre/statement_printer_test.py
@@ -1,6 +1,8 @@
 from doublex import Mimic, Mock
 from doublex_expects import have_been_satisfied
 from expects import expect
+from typing import List
+
 
 from bank.src.domain.transaction import Transaction, TransactionType
 from bank.src.infrastructure.console_output import Console
@@ -19,10 +21,10 @@ class TestStatementPrinter:
     def test_prints_the_header(self) -> None:
         with Mimic(Mock, Console) as console:
             console.print_line("date || credit || debit || balance")
-
         statement_printer = StatementPrinter(console)
+        transactions: List[Transaction] = []
 
-        statement_printer.print([])
+        statement_printer.print(transactions)
 
         expect(console).to(have_been_satisfied)
 
@@ -30,10 +32,10 @@ class TestStatementPrinter:
         with Mimic(Mock, Console) as console:
             console.print_line("date || credit || debit || balance")
             console.print_line("12/01/2012 || 1000.00 || || 1000.00")
-
         statement_printer = StatementPrinter(console)
+        transactions = [deposit("12/01/2012", 1000)]
 
-        statement_printer.print([deposit("12/01/2012", 1000)])
+        statement_printer.print(transactions)
 
         expect(console).to(have_been_satisfied)
 
@@ -41,10 +43,10 @@ class TestStatementPrinter:
         with Mimic(Mock, Console) as console:
             console.print_line("date || credit || debit || balance")
             console.print_line("12/01/2012 || || 1000.00 || -1000.00")
-
         statement_printer = StatementPrinter(console)
+        transactions = [withdraw("12/01/2012", 1000)]
 
-        statement_printer.print([withdraw("12/01/2012", 1000)])
+        statement_printer.print(transactions)
 
         expect(console).to(have_been_satisfied)
 
@@ -55,13 +57,12 @@ class TestStatementPrinter:
             console.print_line("13/01/2012 || || 100.00 || 900.00")
             console.print_line("12/01/2012 || 1000.00 || || 1000.00")
         statement_printer = StatementPrinter(console)
+        transactions = [
+            deposit("12/01/2012", 1000),
+            withdraw("13/01/2012", 100),
+            deposit("14/01/2012", 500),
+        ]
 
-        statement_printer.print(
-            [
-                deposit("12/01/2012", 1000),
-                withdraw("13/01/2012", 100),
-                deposit("14/01/2012", 500),
-            ]
-        )
+        statement_printer.print(transactions)
 
         expect(console).to(have_been_satisfied)

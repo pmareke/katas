eb5a6a75c4ca90cfd56c20d6aedfd9400e1ddf88
Pedro Lopez Mareque <pedro.lopez@clarity.ai>
2022-09-14 14:40:49 +0200

    feat: improve Ohce kata tests (WIP)

--- ohce/tests/ohce_test.py
+++ ohce/tests/ohce_test.py
@@ -1,20 +1,16 @@
-from datetime import datetime
-from doublex import Stub, Mimic, Spy
+from doublex import Mimic, Spy
 from doublex_expects import have_been_called_with
 from expects import expect, contain
 
 from ohce.src.ohce_runner import Ohce
-from ohce.src.custom_clock import CustomClock
 from ohce.src.console_output import ConsoleOutput
+from ohce.tests.test_data import TestData
 
 
 class TestOhce:
     def test_greets_good_night(self) -> None:
         console_output = Mimic(Spy, ConsoleOutput)
-        with Mimic(Stub, CustomClock) as custom_clock:
-            date_time_str = "18/09/19 01:55:19"
-            date = datetime.strptime(date_time_str, "%d/%m/%y %H:%M:%S")
-            custom_clock.time().returns(date)
+        custom_clock = TestData.a_custom_clock("18/09/19 01:55:19")
         ohce = Ohce(console_output, custom_clock)
 
         ohce.run("any-user-name")
@@ -23,10 +19,7 @@ class TestOhce:
 
     def test_greets_good_morning(self) -> None:
         console_output = Mimic(Spy, ConsoleOutput)
-        with Mimic(Stub, CustomClock) as custom_clock:
-            date_time_str = "18/09/19 08:55:19"
-            date = datetime.strptime(date_time_str, "%d/%m/%y %H:%M:%S")
-            custom_clock.time().returns(date)
+        custom_clock = TestData.a_custom_clock("18/09/19 08:55:19")
         ohce = Ohce(console_output, custom_clock)
 
         ohce.run("any-user-name")
@@ -35,10 +28,7 @@ class TestOhce:
 
     def test_greets_good_afternoon(self) -> None:
         console_output = Mimic(Spy, ConsoleOutput)
-        with Mimic(Stub, CustomClock) as custom_clock:
-            date_time_str = "18/09/19 18:55:19"
-            date = datetime.strptime(date_time_str, "%d/%m/%y %H:%M:%S")
-            custom_clock.time().returns(date)
+        custom_clock = TestData.a_custom_clock("18/09/19 18:55:19")
         ohce = Ohce(console_output, custom_clock)
 
         ohce.run("any-user-name")
---
+++ ohce/tests/test_data.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+from doublex import Mimic, Stub, when
+from ohce.src.custom_clock import CustomClock
+
+
+class TestData:
+    @staticmethod
+    def a_custom_clock(date_time_str: str) -> CustomClock:
+        custom_clock = Mimic(Stub, CustomClock)
+        date = datetime.strptime(date_time_str, "%d/%m/%y %H:%M:%S")
+        when(custom_clock).time().returns(date)
+        return custom_clock
